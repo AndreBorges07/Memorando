6a0a763a6c68ce9c66a20eb090be90ec0c204ce1
André <andre.borges777@gmail.com>
2025-01-30 20:17:53 -0300

    Troquei o caminho da tarefas para "https://json-tarefas-memorando.vercel.app/tarefas". Tive que criar o repositório separado pra funcionar no vercel.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://memorando-vert.vercel.app/api/tarefas' // URL do Vercel
-};
+  apiUrl: 'https://json-tarefas-memorando.vercel.app/tarefas' // URL do Vercel
+}; //fiz o deploy no Vercel só com o json. Usei esse repositório: https://github.com/kitloong/json-server-vercel
